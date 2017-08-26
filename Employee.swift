ea6cfe86cf89fcac1adc8d516090af8fdb28d465
Fateme' Kazemi <fatemekazemi@Fatemes-Mac-mini.local>
2017-08-26 17:13:42 +0430

    first commit

---
+++ Employee.swift
@@ -0,0 +1,24 @@
+//
+//  class.swift
+//  reflectionTest
+//
+//  Created by Fateme' Kazemi on 6/4/1396 AP.
+//  Copyright © 1396 Fateme' Kazemi. All rights reserved.
+//
+
+import Foundation
+
+class Employee {
+    var id: String?
+    var someArray: [Employee]?
+    
+    required init(){
+        
+    }
+    func printName(){
+        
+        //return id!
+    }
+}
+
+
